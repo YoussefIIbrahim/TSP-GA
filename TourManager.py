c0801e1b0882a759aea1e22cc5d6132eac1ae424
youssefiibrahim <youssefibrahim.guc@gmail.com>
2019-03-14 23:35:35 +0100

    Initial commit with some files pre-implemented

---
+++ TourManager.py
@@ -0,0 +1,15 @@
+
+
+
+class TourManager:
+
+    destinationCities = []
+
+    def addCity(self, city):
+        self.destinationCities.append(city)
+
+    def getCity(self, index):
+        return self.destinationCities[index]
+
+    def numberOfCities(self):
+        return len(self.destinationCities)
